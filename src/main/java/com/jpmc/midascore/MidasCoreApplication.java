9c33c1188fbda1b4dd2af2002190418d7834b0e3
Dhanesh Shetty <dhaneshshetty65@gmail.com>
2023-01-17 11:36:56 +0530

    feat:kafka integrated

--- src/main/java/com/jpmc/midascore/MidasCoreApplication.java
+++ src/main/java/com/jpmc/midascore/MidasCoreApplication.java
@@ -2,12 +2,38 @@ package com.jpmc.midascore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+
+import com.jpmc.midascore.foundation.Transaction;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class MidasCoreApplication {
+    @Value("${general.kafka-topic}") 
+    private String topicName;
 
     public static void main(String[] args) {
+        System.out.println("Started");
         SpringApplication.run(MidasCoreApplication.class, args);
+        
+    }
+
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name(topicName)
+        .partitions(1)
+        .replicas(1)
+        .build();
+    }
+
+    @KafkaListener(topics = "topic1",
+    containerFactory = "kafkaListenerContainerFactory",groupId = "group_id")
+    public void listen(Transaction transaction){
+        System.out.println(transaction);
     }
 
 }
--- src/test/java/com/jpmc/midascore/KafkaProducer.java
+++ src/test/java/com/jpmc/midascore/KafkaProducer.java
@@ -1,22 +1,47 @@
 package com.jpmc.midascore;
 
 import com.jpmc.midascore.foundation.Transaction;
+
+import java.util.*;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaProducer {
-    private final String topic;
-    private final KafkaTemplate<String, Transaction> kafkaTemplate;
+    @Bean
+    public ProducerFactory<String, Transaction> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
 
-    public KafkaProducer(@Value("${general.kafka-topic}") String topic, KafkaTemplate<String, Transaction> kafkaTemplate) {
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+
+    @Bean
+    public KafkaTemplate<String, Transaction> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+    private final String topic;
+    private final KafkaTemplate<String,Transaction> temp;
+
+    public KafkaProducer(@Value("${general.kafka-topic}") String topic) {
         this.topic = topic;
-        this.kafkaTemplate = kafkaTemplate;
+        this.temp = kafkaTemplate();
     }
 
     public void send(String transactionLine) {
         String[] transactionData = transactionLine.split(", ");
-        kafkaTemplate.send(topic, new Transaction(Long.parseLong(transactionData[0]), Long.parseLong(transactionData[1]), Float.parseFloat(transactionData[2])));
+        temp.send(topic, new Transaction(Long.parseLong(transactionData[0]), Long.parseLong(transactionData[1]), Float.parseFloat(transactionData[2])));
     }
 }
